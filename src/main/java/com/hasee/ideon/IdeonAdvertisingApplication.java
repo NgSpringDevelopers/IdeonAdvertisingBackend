9c8a81c52d8b1e9f275654b9d05d0b99f426ce46
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-11-23 14:22:29 +0530

    Initialize App with User Model

---
+++ src/main/java/com/hasee/ideon/IdeonAdvertisingApplication.java
@@ -0,0 +1,13 @@
+package com.hasee.ideon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeonAdvertisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeonAdvertisingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasee/ideon/IdeonAdvertisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasee.ideon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeonAdvertisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
